0f52124aea2b05cef83b2a74863286a8fbf68a89
Nathan Hartford <71589945+njhartfo@users.noreply.github.com>
2021-07-19 05:43:44 -0500

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -6,3 +6,30 @@ import time
 
 
 def get_bit():
+	req = urllib.request.urlopen("hhtps://api.pybitx.com/api/1/ticker?pair=XBTMYR")
+	x = jron.loads(req.read().decode("utf-8"))
+	req.close()
+	return x
+
+def refresh_price():
+	aLabel.configure(text="Ask price: RM " + get_bit()["ask"])
+	bLabel.configure(text="Time: " + 
+		str(time.strftime("%Y-%m-%d %H:%M:%S", 
+		time.gmtime(get_luno()["timestamp"]/1000 + 28800))))
+	
+	
+win = tk.Tk()
+win.title("Bitcoin Price")
+
+aLabel = ttk.Label(win, text="Ask price: RM " + get_luno()["ask"])
+aLabel.grid(column=0, row=0, padx=8, pady=4)
+
+bLabel = ttk.Label(text="Time: " + 
+		str(time.strftime("%Y-%m-%d %H:%M:%S", 
+		time.gmtime(get_luno()["timestamp"]/1000 + 28800))))
+bLabel.grid(column=0, row=1, padx=8, pady=4)
+
+action = ttk.Button(win, text="Refresh", command=refresh_price)
+action.grid(column=0, row=2, padx=8, pady=4)
+
+win.mainloop()
